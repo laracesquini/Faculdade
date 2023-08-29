5abcf17c0499a98d35dff56b185a45beffd98705
laracesquini <laracesquini@gmail.com>
2023-08-29 09:05:34 -0300

    começando

--- Segundo ano/2 semestre/Estrutura de Dados II/Projeto1/projeto.c
+++ Segundo ano/2 semestre/Estrutura de Dados II/Projeto1/projeto.c
@@ -3,17 +3,20 @@
 #include <string.h>
 #include "arquivos.h"
 
+//0 - desatualizado
+//1 - atualizado
+
 int main()
 {
      int flagp, flags, i = 0, j = 0;
-     char aux[20];
+     char aux[192], *token;
      FILE *fd, *fp, *fs;
      Iprimario vetp[1000];
      Isecundario vets[1000];
 
-     if(file_exists("dados.txt"))
+     if(file_exists("movies.dat"))
      {
-        fd = fopen("dados.txt", "a+");
+        fd = fopen("movies.dat", "a+");
         if(file_exists("iprimary.idx") && file_exists("ititle.idx"))
         {
           fp = fopen("iprimary.idx", "a+");
@@ -27,7 +30,7 @@ int main()
                {
                     fscanf(fp, " %[^#]s", aux);
                     fgetc(fp);
-                    char *token = strtok(aux, "@");
+                    token = strtok(aux, "@");
                     strcpy(vetp[i].first_key, token);
                     token = strtok(NULL, "@");
                     vetp[i].RNN = atoi(token);
@@ -45,7 +48,7 @@ int main()
                {
                     fscanf(fs, " %[^#]s", aux);
                     fgetc(fs);
-                    char *token = strtok(aux, "@");
+                    token = strtok(aux, "@");
                     strcpy(vets[j].titulo, token);
                     token = strtok(NULL, "@");
                     strcpy(vets[j].first_key, token);
@@ -58,17 +61,32 @@ int main()
           }
         }
         else
-        {
-          fp = fopen("iprimary.idx", "w");
-          fs = fopen("ititle.idx", "w");
+        { //criar os indices nos vetores e salvar no arquivo -> ainda vou ter os indices em RAM, e em qualquer alteração, muda a flag para 0.
+          fp = fopen("iprimary.idx", "a+");
+          fs = fopen("ititle.idx", "a+");
+          fd = fopen("movies.dat", "a+");
 
+          while(!feof(fd))
+          {
+               fread(aux, 192, 1, fd);
+               token = strtok(aux, "@");
+               strcpy(vetp[i].first_key, token);
+               strcpy(vets[j].first_key, token);
+               token = strtok(NULL, "@");
+               strcpy(vets[j].titulo, token);
+               vetp[i].RNN = i;
+               i++;
+               j++;
+          }
+          //ordenar pela chave
           fprintf(fp, "%d\n", 1);
           fprintf(fs, "%d\n", 1);
+          //salvar no arquivo
         }
      }
      else
      {
-        fd = fopen("dados.txt", "a+");
+        fd = fopen("movies.dat", "a+");
      }
 
      //fechar os arquivos
