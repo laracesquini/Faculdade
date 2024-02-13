95a277a6ddbef7c4e25c5c4b421432735dd19fab
laracesquini <laracesquini@gmail.com>
2024-02-13 17:26:27 -0300

    Calculo dos vetores de similaridade e alcance

--- Iniciação científica/Projeto/paraconsistent_logic.c
+++ Iniciação científica/Projeto/paraconsistent_logic.c
@@ -82,13 +82,55 @@ void normaliza_matriz(double **matriz)
     return;
 }
 
+double *calculo_vetor_similaridade(double **matriz)
+{
+    double *vetor, maior, menor;
+    int i, j;
+
+    vetor = (double*)malloc(colunas * sizeof(double)); 
+
+    for(j = 0; j < colunas; j++)
+    {
+        maior = 0;
+        menor = 1;
+        for(i = 0; i < linhas; i++)
+        {
+            if(matriz[i][j] > maior)
+            maior = matriz[i][j];
+
+            if(matriz[i][j] < menor)
+            menor = matriz[i][j];
+        }
+        vetor[j] = maior - menor;
+    }
+
+    for(j = 0; j < colunas; j++)
+    {
+        vetor[j] = 1 - vetor[j];
+    }
+
+    return vetor;
+}
+
+void calculo_vetor_alcance(double **matriz, double *maior, double *menor)
+{
+
+}
 int main()
 {
-    double **classe_patologicas, **classe_nao_patologicas;
+    double **classe_patologicas, **classe_nao_patologicas, *vetor_similaridade_1, *vetor_similaridade_2, *menores_valores_1, *maiores_valores_1, *menores_valores_2, *maiores_valores_2;
+
     classe_patologicas = ler_dados("caracteristicas_patologicas.txt");
     classe_nao_patologicas = ler_dados("caracteristicas_nao_patologicas.txt");
 
     normaliza_matriz(classe_patologicas);
     normaliza_matriz(classe_nao_patologicas);
+
+    vetor_similaridade_1 = calculo_vetor_similaridade(classe_patologicas);
+    vetor_similaridade_2 = calculo_vetor_similaridade(classe_nao_patologicas);
+
+    //menores_valores_1 = menores_valores_2 = maiores_valores_1 = maiores_valores_2 = (double*)malloc(colunas * sizeof(double));
+
+    
     
 }
