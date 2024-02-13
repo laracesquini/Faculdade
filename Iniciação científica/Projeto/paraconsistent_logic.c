195c27d979622a9bf7e15165edc7bcb5496b489a
laracesquini <laracesquini@gmail.com>
2024-02-13 18:21:30 -0300

    Atualizando
    Conclusão da análise intraclasse e início da análise interclasse.

--- Iniciação científica/Projeto/paraconsistent_logic.c
+++ Iniciação científica/Projeto/paraconsistent_logic.c
@@ -4,6 +4,53 @@
 #define linhas 30
 #define colunas 7
 
+void imprimir_matriz(double **matriz);
+double modulo(double num);
+double **ler_dados(char *nome);
+void normaliza_matriz(double **matriz);
+double *calculo_vetor_similaridade(double **matriz);
+void calculo_vetor_alcance(double **matriz, double *maior, double *menor);
+double media(double *vet);
+
+int main()
+{   
+    double **classe_patologicas, **classe_nao_patologicas; //declaração de matrizes
+    double *vetor_similaridade_1, *vetor_similaridade_2, *menores_valores_1, *maiores_valores_1, *menores_valores_2, *maiores_valores_2; //declaração de vetores
+    double alpha, aux; //declaração de variáveis
+
+    //leitura dos dados
+    classe_patologicas = ler_dados("caracteristicas_patologicas.txt");
+    classe_nao_patologicas = ler_dados("caracteristicas_nao_patologicas.txt");
+
+    //normalização dos vetores de características
+    normaliza_matriz(classe_patologicas);
+    normaliza_matriz(classe_nao_patologicas);
+
+    //intraclass analysis
+    //cálculo do vetor de similaridade para cada classe
+    vetor_similaridade_1 = calculo_vetor_similaridade(classe_patologicas);
+    vetor_similaridade_2 = calculo_vetor_similaridade(classe_nao_patologicas);
+
+    //cálculo do alpha
+    alpha = media(vetor_similaridade_1);
+    aux = media(vetor_similaridade_2);
+
+    if(alpha > aux)
+    alpha = aux;
+    
+    //interclass analysis
+    //cálculo dos vetores de alcance
+    menores_valores_1 = (double*)malloc(colunas * sizeof(double));
+    menores_valores_2 = (double*)malloc(colunas * sizeof(double));
+    maiores_valores_1 = (double*)malloc(colunas * sizeof(double));
+    maiores_valores_2 = (double*)malloc(colunas * sizeof(double));
+
+    calculo_vetor_alcance(classe_patologicas, &maiores_valores_1[0], &menores_valores_1[0]);
+    calculo_vetor_alcance(classe_nao_patologicas, &maiores_valores_2[0], &menores_valores_2[0]);
+
+    //cálculo do R
+}
+
 void imprimir_matriz(double **matriz)
 {
     int i, j;
@@ -114,23 +161,39 @@ double *calculo_vetor_similaridade(double **matriz)
 
 void calculo_vetor_alcance(double **matriz, double *maior, double *menor)
 {
+    int i, j;
+    double maior_valor, menor_valor;
 
+    for(j = 0; j < colunas; j++)
+    {
+        maior_valor = 0;
+        menor_valor = 1;
+        for(i = 0; i < linhas; i++)
+        {
+            if(matriz[i][j] > maior_valor)
+            maior_valor = matriz[i][j];
+
+            if(matriz[i][j] < menor_valor)
+            menor_valor = matriz[i][j];
+        }
+        maior[j] = maior_valor;
+        menor[j] = menor_valor;
+    }
+
+    return;  
 }
-int main()
+
+double media(double *vet)
 {
-    double **classe_patologicas, **classe_nao_patologicas, *vetor_similaridade_1, *vetor_similaridade_2, *menores_valores_1, *maiores_valores_1, *menores_valores_2, *maiores_valores_2;
+    int i;
+    double media = 0;
 
-    classe_patologicas = ler_dados("caracteristicas_patologicas.txt");
-    classe_nao_patologicas = ler_dados("caracteristicas_nao_patologicas.txt");
+    for(i = 0; i < colunas; i++)
+    {
+        media = media + vet[i];
+    }
 
-    normaliza_matriz(classe_patologicas);
-    normaliza_matriz(classe_nao_patologicas);
+    media = media/colunas;
 
-    vetor_similaridade_1 = calculo_vetor_similaridade(classe_patologicas);
-    vetor_similaridade_2 = calculo_vetor_similaridade(classe_nao_patologicas);
-
-    //menores_valores_1 = menores_valores_2 = maiores_valores_1 = maiores_valores_2 = (double*)malloc(colunas * sizeof(double));
-
-    
-    
+    return media;
 }
