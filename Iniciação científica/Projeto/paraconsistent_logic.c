268367444d3f842800e18b07cd1d83bab74a3010
laracesquini <laracesquini@gmail.com>
2024-02-15 10:44:38 -0300

    Finalização

--- Iniciação científica/Projeto/paraconsistent_logic.c
+++ Iniciação científica/Projeto/paraconsistent_logic.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
+
 #define linhas 30
 #define colunas 7
+
+typedef struct ponto
+{
+    double x;
+    double y;
+}Ponto;
 
 void imprimir_matriz(double **matriz);
 double modulo(double num);
@@ -12,12 +20,14 @@ double *calculo_vetor_similaridade(double **matriz);
 void calculo_vetor_alcance(double **matriz, double *maior, double *menor);
 double media(double *vet);
 double overlap(double **classe1, double **classe2, double *maiores_valores_1, double *menores_valores_1, double *maiores_valores_2, double *menores_valores_2);
+double distancia_entre_pontos(Ponto P, double x, double y);
 
 int main()
 {   
     double **classe_patologicas, **classe_nao_patologicas; //declaração de matrizes
     double *vetor_similaridade_1, *vetor_similaridade_2, *menores_valores_1, *maiores_valores_1, *menores_valores_2, *maiores_valores_2; //declaração de vetores
-    double alpha, aux, R; //declaração de variáveis
+    double alpha, aux, R, F, beta, G1, G2, d1, d2, d3, d4; //declaração de variáveis
+    Ponto P;
 
     //leitura dos dados
     classe_patologicas = ler_dados("caracteristicas_patologicas.txt");
@@ -48,22 +58,35 @@ int main()
 
     calculo_vetor_alcance(classe_patologicas, &maiores_valores_1[0], &menores_valores_1[0]);
     calculo_vetor_alcance(classe_nao_patologicas, &maiores_valores_2[0], &menores_valores_2[0]);
-    printf("\nClasse 1\nSimilaridade: ");
-    for(int i = 0; i < colunas; i++)
-    printf("%.5f, ", vetor_similaridade_1[i]);
-
-    printf("\nMaiores");
-    for(int i = 0; i < colunas; i++)
-    printf("%.5f, ", maiores_valores_1[i]);
-
-    printf("\nMenores: ");
-    for(int i = 0; i < colunas; i++)
-    printf("%.5f, ", menores_valores_1[i]);
-
-    //cálculo do R
+    
+    //cálculo do beta
     R = overlap(classe_patologicas, classe_nao_patologicas, maiores_valores_1, menores_valores_1, maiores_valores_2, menores_valores_2);
+    F = 2*1*linhas*colunas;
+    beta = R/F;
 
-    printf("R: %f", R);
+    //Ponto P
+    G1 = alpha - beta;
+    G2 = alpha + beta - 1;
+    P.x = G1;
+    P.y = G2;
+    
+    printf("Beta: %f\nAlpha:%f\nG1: %f\nG2: %f\n", beta, alpha, G1, G2);
+
+    //Cálculo das distâncias
+    //d1((G1, G2), (-1, 0))
+    d1 = distancia_entre_pontos(P, -1, 0);
+
+    //d2((G1, G2), (1, 0))
+    d2 = distancia_entre_pontos(P, 1, 0);
+
+    //d3((G1, G2), (0, -1))
+    d3 = distancia_entre_pontos(P, 0, -1);
+
+    //d4((G1, G2), (0, 1))
+    d4 = distancia_entre_pontos(P, 0, 1);
+
+    printf("Distâncias: \nd1: %f\nd2: %f\nd3: %f\nd4: %f", d1, d2, d3, d4);
+
 }
 
 void imprimir_matriz(double **matriz)
@@ -237,4 +260,14 @@ double overlap(double **classe1, double **classe2, double *maiores_valores_1, do
     }
 
     return R;
+}
+
+double distancia_entre_pontos(Ponto P, double x, double y)
+{
+    double dist;
+
+    dist = pow((P.x - x), 2) + pow((P.y - y), 2);
+    dist = sqrt(dist);
+
+    return dist;
 }
