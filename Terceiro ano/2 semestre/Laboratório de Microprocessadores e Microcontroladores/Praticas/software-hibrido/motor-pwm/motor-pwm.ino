2d7a81a88dfab4b312b7e41b3245ebc2f82f16e9
laracesquini <laracesquini@gmail.com>
2024-09-16 19:43:36 -0300

    Atualizando
    Finalmente funcionando

--- Terceiro ano/2 semestre/Laboratório de Microprocessadores e Microcontroladores/Praticas/software-hibrido/motor-pwm/motor-pwm.ino
+++ Terceiro ano/2 semestre/Laboratório de Microprocessadores e Microcontroladores/Praticas/software-hibrido/motor-pwm/motor-pwm.ino
@@ -1,20 +1,14 @@
 #include "asmfunc.h"
 
-int pino_motor = 8;
-int botao3 = 5;
-int botao2 = 6;
-int botao1 = 7;
-
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(7, OUTPUT);
-  digitalWrite(7, LOW);
-
-  asmfunc();
-}
+//definição das conexões dos componentes com o arduíno
+int pino_motor = 7;  
+int botao1 = 4;      
+int botao2 = 5;      
+int botao3 = 6;      
 
 void setup()
 {
+  //definição do motor como saída e dos botões como entrada
   pinMode(pino_motor, OUTPUT);
   pinMode(botao1, INPUT);
   pinMode(botao2, INPUT);
@@ -23,21 +17,20 @@ void setup()
 
 void loop()
 {
+  //leitura das entradas fornecidas pelos botões
   int estadoBotao1 = digitalRead(botao1);
   int estadoBotao2 = digitalRead(botao2);
   int estadoBotao3 = digitalRead(botao3);
-
+  
+  //verifica qual botão foi pressionado e faz a chamada da função em Assembly passando essa informação
   if (estadoBotao1 == 1) {
-    digitalWrite(pino_motor, HIGH);
-    delay(1000);
+	asmfunc(1);
   } 
   else if(estadoBotao2 == 1){
-    digitalWrite(pino_motor, HIGH);
-    delay(5000);
+	asmfunc(2);
   }
   else if(estadoBotao3 == 1){
-    digitalWrite(pino_motor, HIGH);
-    delay(10000);
+	asmfunc(3);
   }
   else{
     digitalWrite(pino_motor, LOW);
